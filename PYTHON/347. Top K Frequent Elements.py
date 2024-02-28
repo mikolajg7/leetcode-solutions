94cc706968da174da1eab56cb63d11ff7f3a952b
Mikolaj Golanski <mikolajgolanski16@gmail.com>
2024-02-28 20:22:04 +0100

    Added solution to task 347. Top K Frequent Elements

---
+++ PYTHON/347. Top K Frequent Elements.py
@@ -0,0 +1,7 @@
+from collections import Counter
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        answ=[]
+        sort=Counter(nums)  # returns dictionary of numbers with their quantity
+        sorted_nums=sorted(sort, key=lambda x: sort[x], reverse=True)  # sorting by frequency
+        return list(sorted_nums)[:k]
