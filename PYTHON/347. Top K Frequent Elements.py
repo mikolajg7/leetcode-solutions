d2a3ad70ede6903b145bd9e69a7bea540a7aa8c9
Mikolaj Golanski <mikolajgolanski16@gmail.com>
2024-03-02 23:26:46 +0100

    Marge remote changes

--- PYTHON/347. Top K Frequent Elements.py
+++ PYTHON/347. Top K Frequent Elements.py
@@ -3,5 +3,5 @@ class Solution(object):
     def topKFrequent(self, nums, k):
         answ=[]
         sort=Counter(nums)  # returns dictionary of numbers with their quantity
-        sorted_nums=sorted(sort, key=lambda x: sort[x], reverse=True)  # sorting by frequency
+        sorted_nums=sorted(sort, key=lambda x: sort[x], reverse=True)  # sorting by frequency  
         return list(sorted_nums)[:k]
--- README.md
+++
@@ -1,1 +0,0 @@
-Solutions to problems from the leetcode website based on the roadmap: https://neetcode.io/roadmap
