b75119beed5320b6373171fe35891c90d2a337ed
Mikołaj Golański <148275198+mikolajg7@users.noreply.github.com>
2024-03-02 23:31:09 +0100

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-kwadraty = []
-for i in range(0, 9, 3):  # iteruj co 3, ponieważ są 3 kolumny w każdym kwadracie
-    for j in range(0, 9, 3):  # iteruj co 3, ponieważ są 3 wiersze w każdym kwadracie
-        kwadrat = [row[j:j+3] for row in sudoku[i:i+3]]
-        kwadraty.append(kwadrat)
